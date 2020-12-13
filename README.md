028f28591c4718a21448be450e9f3c75ea46851d
mahale <mahale@adobe.com>
2020-12-13 00:20:10 -0800

    shiny app

--- README.md
+++ README.md
@@ -1,12 +1,28 @@
-# BookRecommender
+# Movie Recommender
 
-This is code for a shiny app for a book recommender based on collaborative filtering. 
+This is code for a shiny app for a movie recommender based on 2 systems - Favorite Genre & Collaborative Filtering. 
 
-For more information: See this [kernel](https://www.kaggle.com/philippsp/book-recommender-collaborative-filtering-shiny) on Kaggle.com.
+### Systems I & II
+System I - Favorite Genre
 
-Credits:
-My thanks go to the following persons:
-1. Stefan Nicklic for his [improved implementation of collaborative filtering](https://github.com/smartcat-labs/collaboratory).
-2. Stefan Wilhelm for providing [ShinyRatingInput](https://github.com/stefanwilhelm/ShinyRatingInput).
-3. Dean Attali for his work on [shinyJS](https://github.com/daattali/shinyjs).
-4. Kaggle user [Foxtrot](https://www.kaggle.com/zygmunt) for providing the [goodbooks dataset](https://www.kaggle.com/zygmunt/goodbooks-10k).
+By default, the System I View will be displayed when the app is run. You can select a genre from the dropdown and click on the button to get recommendations. The movies that are highly rated (With ratings of 4 stars or 5 stars) in the selected genre are recommended.
+
+System II - Collaborative filtering. 
+
+To navigate to System II, go from the left menu and you will be presented with few movies that you can rate. Once you have rated as many movies you want, click on the button below to get your recommendations.
+Here the  User-Based Collaborative filtering algorithm is used.
+
+### Dataset
+This app is using the Movielense Data. The dataset contains about 1 million anonymous ratings of approximately 3,900 movies made by 6,040 MovieLens users who joined MovieLens in 2000.
+
+## How to run the App
+https://github.com/PunamSmahale/movie-recommender-shiny-app
+
+You will need RStudio IDE. Download/clone the above repo and open the ui.R or server.R file in IDE and click on Run App  (top-right corner.). The app would open in RStudio browser.
+
+Also, the app is hosted on shinyapps.io - https://mahale.shinyapps.io/project4-shiny-app/
+
+## Acknowledgements
+1. UIUC CS 598 Team of professors and instructors. 
+2. https://www.kaggle.com/philippsp/book-recommender-collaborative-filtering-shiny
+3. https://github.com/smartcat-labs/collaboratory
--- ui.R
+++ ui.R
@@ -1,3 +1,13 @@
+
+
+# Load required R packages
+mypackages = c("shiny", "shinydashboard", "recommenderlab", "data.table", "ShinyRatingInput", "shinyjs", "dplyr", "tidyr")
+tmp = setdiff(mypackages, rownames(installed.packages()))  # packages need to be installed
+if (length(tmp) > 0) install.packages(tmp)
+lapply(mypackages, require, character.only = TRUE)
+#install.packages("devtools")
+#devtools::install_github("stefanwilhelm/ShinyRatingInput")
+
 ## ui.R
 library(shiny)
 library(shinydashboard)
@@ -5,37 +15,79 @@ library(recommenderlab)
 library(data.table)
 library(ShinyRatingInput)
 library(shinyjs)
+library(dplyr)
+library(tidyr)
 
 source('functions/helpers.R')
 
-shinyUI(
-    dashboardPage(
-          skin = "blue",
-          dashboardHeader(title = "Book Recommender"),
-          
-          dashboardSidebar(disable = TRUE),
+all_genres = c("Action", "Adventure", "Animation", "Children", 
+               "Comedy", "Crime","Documentary", "Drama", "Fantasy", "Film-Noir",
+               "Horror", "Musical", "Mystery","Romance",
+               "Sci-Fi", "Thriller", "War", "Western")
 
-          dashboardBody(includeCSS("css/books.css"),
-              fluidRow(
-                  box(width = 12, title = "Step 1: Rate as many books as possible", status = "info", solidHeader = TRUE, collapsible = TRUE,
-                      div(class = "rateitems",
-                          uiOutput('ratings')
-                      )
+shinyUI(
+  dashboardPage(skin = "yellow",
+                dashboardHeader(title = "Movie Recommender"),
+                dashboardSidebar(
+                  tags$head(
+                    tags$link(rel = "stylesheet", type = "text/css", href = "css/books.css")
+                  ),
+                  sidebarMenu(
+                    menuItem("System I", tabName="first",  icon=icon("star")),
+                    menuItem("System II", tabName = "second",  icon=icon("star"))
                   )
                 ),
-              fluidRow(
-                  useShinyjs(),
-                  box(
-                    width = 12, status = "info", solidHeader = TRUE,
-                    title = "Step 2: Discover books you might like",
-                    br(),
-                    withBusyIndicatorUI(
-                      actionButton("btn", "Click here to get your recommendations", class = "btn-warning")
+                dashboardBody(
+                  tabItems(
+                    tabItem(tabName = "first",
+                            fluidRow(
+                              box(width = 12,title = "Step 1: Select Your Favorite Genre", status = "info", solidHeader = TRUE, collapsible = TRUE,
+                                  div(class = "genres",
+                                        selectInput("selected_genre", "Select a Genre ", all_genres),
+                                  )
+                              )
+                            ),
+                            fluidRow(
+                              useShinyjs(),
+                              
+                              box(
+                                width = 12, status = "info", solidHeader = TRUE,
+                                title = "Step 2: Get Recommmended Movies For Selected Favorite Genre",
+                                br(),
+                                withBusyIndicatorUI(
+                                  actionButton("btn_genre_movies", "Click to get your recommendations!!", class = "btn-warning")
+                                 
+                                ),
+                                h4("Note: For the selected genre, highly rated movies are recommended here. These movies have 4 or 5 stars."),
+                                br(),
+                                tableOutput("selectedGenreMovies")
+                              )
+                            )
                     ),
-                    br(),
-                    tableOutput("results")
+                    
+                    tabItem(tabName = "second",
+                            fluidRow(
+                              box(width = 12, title = "Step 1: Rate as many movies as possible", status = "info", solidHeader = TRUE, collapsible = TRUE,
+                                  div(class = "rateitems",
+                                      uiOutput('ratings')
+                                  ))
+                            ),
+                            fluidRow(
+                              useShinyjs(),
+                              box(
+                                width = 12, status = "info", solidHeader = TRUE,
+                                title = "Step 2: Discover Movies you might like",
+                                br(),
+                                withBusyIndicatorUI(
+                                  actionButton("btn_rated_movies", "Click here to get your recommendations", class = "btn-warning")
+                                ),
+                                h4("Note: User-based collaborative filtering algorithm is used to recommend the movies."),
+                                br(),
+                                tableOutput("results")
+                              )
+                            )
+                    )
                   )
-               )
-          )
-    )
+                )
+  )
 ) 
